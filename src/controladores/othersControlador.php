4974e78e0e8f62f1d6529dfdb4e65cb0e58ef0c2
Anderson <andersonfreitez6@gmail.com>
2026-08-15 18:40:27 -0400

    Modifique el controlador de others para que no arrojara error en el buscar de la vista de home.php

--- src/controladores/othersControlador.php
+++ src/controladores/othersControlador.php
@@ -3,12 +3,17 @@
 use src\config\inc\componentesModelo;
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
   $archivo = "src/vistas/others/";
-  if (isset($url2) && $url2 != "") {
-    
-    if (is_file($archivo . $url2 . ".php")) {
-      $archivo .= $url2 . ".php";
-      $_SESSION['vistaActual'] = $url2;
+  if (isset($_SESSION['url2']) && $_SESSION['url2'] != "") {
+    if (is_file($archivo . $_SESSION['url2'] . ".php")) {
+      $archivo .= $_SESSION['url2'] . ".php";
+      $_SESSION['vistaActual'] = $_SESSION['url2'];
+    }
+  }elseif (isset($_SESSION['url1']) && $_SESSION['url1'] != "") {
+    if (is_file($archivo . $_SESSION['url1'] . ".php")) {
+      $archivo .= $_SESSION['url1'] . ".php";
+      $_SESSION['vistaActual'] = $_SESSION['url1'];
     }
   }
   $objComponentes = new componentesModelo();
